f5a41578496dc3ad37760a05114210a4bca41797
Wesley <wesleyrogerio77@gmail.com>
2025-06-15 16:00:24 -0300

    fix

--- buscar-cursos.php
+++ buscar-cursos.php
@@ -12,9 +12,10 @@ $crawler = new Crawler();
 $buscador = new Buscador($client, $crawler);
 $cursos = $buscador->buscar(url:'cursos-online-programacao');
 
-echo "Lista de cursos encontrados (" . count($cursos) . "):" . PHP_EOL;
-$indice = 1;
-foreach ($cursos as $curso) {
-    echo "#" . $indice . " - " . $curso . PHP_EOL;
-    $indice++;
+$totalCursos = count($cursos);
+echo "Lista de cursos encontrados ($totalCursos):" . PHP_EOL;
+
+foreach ($cursos as $indice => $curso) {
+    exibeMensagem($curso);
 }
+
