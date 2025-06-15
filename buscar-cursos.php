4d1db2e520e4fd3deb9c0b5db9b6446a40b517e8
Wesley <wesleyrogerio77@gmail.com>
2025-06-15 15:21:53 -0300

    fix: indice para lista de curso

--- buscar-cursos.php
+++ buscar-cursos.php
@@ -12,6 +12,9 @@ $crawler = new Crawler();
 $buscador = new Buscador($client, $crawler);
 $cursos = $buscador->buscar(url:'cursos-online-programacao');
 
+echo "Lista de cursos encontrados (" . count($cursos) . "):" . PHP_EOL;
+$indice = 1;
 foreach ($cursos as $curso) {
-    echo exibeMensagem($curso);
+    echo "#" . $indice . " - " . $curso . PHP_EOL;
+    $indice++;
 }
